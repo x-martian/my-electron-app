245e32dc103b00c48b75cab20b478e638914d07b
Xiaofeng Zhao <xzhao@viewray.com>
2023-10-11 22:09:05 -0600

    second commit

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
 
 const createMainWindow = () => {
     const win = new BrowserWindow({
+        title: "my-electron-app",
         width: 800,
         height: 600
     })
@@ -10,6 +14,79 @@ const createMainWindow = () => {
     win.loadFile(path.join(__dirname, './renderer/index.html'));
 }
 
+// About Window
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+        width: 300,
+        height: 300,
+        title: 'About Electron',
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    });
+
+    aboutWindow.loadFile(path.join(__dirname, './renderer/about.html'));
+}
+
 app.whenReady().then(() => {
-    createMainWindow()
+    createMainWindow();
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    app.on('activate', ()=>{
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
+})
+
+// Menu template
+const menu = [
+    ...(isMac
+        ? [
+            {
+                label: app.name,
+                submenu: [
+                    {
+                        label: 'About',
+                        click: createAboutWindow,
+                    },
+                ],
+            },
+        ]
+        : []),
+    {
+        role: 'fileMenu',
+    },
+    ...(!isMac
+        ? [
+            {
+                label: 'Help',
+                submenu: [
+                    {
+                        label: 'About',
+                        click: createAboutWindow,
+                    },
+                ],
+            },
+        ]
+        : []),
+    ...(isDev
+        ? [
+            {
+                label: 'Developer',
+                submenu: [
+                    { role: 'reload' },
+                    { role: 'forcereload' },
+                    { type: 'separator' },
+                    { role: 'toggledevtools' },
+                ],
+            },
+        ]
+        : []),
+];
+
+app.on("window-all-closed", ()=>{
+    if (!isMac) {
+        app.quit();
+    }
 })
